4234f533369672a4caef353a5e934a47c43a2e31
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2019-02-03 22:53:28 +0200

    Add mentor fixes

--- index.php
+++ index.php
@@ -76,9 +76,7 @@ $show_complete_tasks = rand(0, 1);
 
                     <label class="checkbox">
                         <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox"
-                            <?php if ($show_complete_tasks): ?> checked <?php endif ?>
-                        >
+                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php echo $show_complete_tasks ? 'checked' : ''; ?>>
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
@@ -112,7 +110,7 @@ $show_complete_tasks = rand(0, 1);
                             <td class="task__controls">
                             </td>
                         </tr>
-                    <?php endif ?>
+                    <?php endif; ?>
                 </table>
             </main>
         </div>
