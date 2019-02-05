2e164baa31c37d7036e1782e35d326e06d6e8770
nigel-riss <mr.kurenkov@gmail.com>
2019-02-05 18:04:56 +0200

    Add projects list

--- index.php
+++ index.php
@@ -5,12 +5,42 @@ $show_complete_tasks = rand(0, 1);
 $projects = ['Входящие', 'Учеба', 'Работа', 'Домашние дела', 'Авто'];
 // Tasks array
 $tasks = [
-    ['Собеседование в IT компании', '01.12.2019', 'Работа', false],
-    ['Выполнить тестовое задание', '25.12.2019', 'Работа', false],
-    ['Сделать задание первого раздела', '21.12.2019', 'Учеба', false],
-    ['Встреча с другом', '22.12.2019', 'Входящие', true],
-    ['Купить корм для кота', 'Нет', 'Домашние дела', false],
-    ['Заказать пиццу', 'Нет', 'Домашние дела', false]
+    [
+        'title' => 'Собеседование в IT компании',
+        'due' => '01.12.2019',
+        'project' => 'Работа',
+        'done' => false
+    ],
+    [
+        'title' => 'Выполнить тестовое задание',
+        'due' => '25.12.2019',
+        'project' => 'Работа',
+        'done' => false
+    ],
+    [
+        'title' => 'Сделать задание первого раздела',
+        'due' => '21.12.2019',
+        'project' => 'Учеба',
+        'done' => false
+    ],
+    [
+        'title' => 'Встреча с другом',
+        'due' => '22.12.2019',
+        'project' => 'Входящие',
+        'done' => true
+    ],
+    [
+        'title' => 'Купить корм для кота',
+        'due' => NULL,
+        'project' => 'Домашние дела',
+        'done' => false
+    ],
+    [
+        'title' => 'Заказать пиццу',
+        'due' => NULL,
+        'project' => 'Домашние дела',
+        'done' => false
+    ]
 ];
 ?>
 <!DOCTYPE html>
@@ -57,10 +87,12 @@ $tasks = [
 
                 <nav class="main-navigation">
                     <ul class="main-navigation__list">
-                        <li class="main-navigation__list-item">
-                            <a class="main-navigation__list-item-link" href="#">Название проекта</a>
-                            <span class="main-navigation__list-item-count">0</span>
-                        </li>
+                        <?php foreach ($projects as $key => $project): ?>
+                            <li class="main-navigation__list-item">
+                                <a class="main-navigation__list-item-link" href="#"><?=$project;?></a>
+                                <span class="main-navigation__list-item-count">0</span>
+                            </li>
+                        <?php endforeach; ?>
                     </ul>
                 </nav>
 
@@ -87,7 +119,7 @@ $tasks = [
 
                     <label class="checkbox">
                         <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php echo $show_complete_tasks ? 'checked' : ''; ?>>
+                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?=$show_complete_tasks ? 'checked' : '';?>>
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
