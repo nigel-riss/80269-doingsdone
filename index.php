7f4f8005217f012e602e2bb67558fa62e72a06fd
nigel-riss <mr.kurenkov@gmail.com>
2019-02-25 17:01:05 +0200

    Add project fetch from db

--- index.php
+++ index.php
@@ -4,12 +4,28 @@ date_default_timezone_get('Etc/GMT');
 require_once('functions.php');
 require_once('data.php');
 
+$user_id = 1;
+
 $connection = mysqli_connect('localhost', 'root', '', 'doingsdone');
 
 $con_status = '';
 if ($connection == false) {
     echo 'Oшибка подключения: ' . mysqli_connect_error();
 } else {
+    mysqli_set_charset($connection, 'utf8');
+
+    $projects_sql = "SELECT * FROM projects WHERE `author_id` =" . $user_id;
+    // $projects_sql = "SELECT * FROM projects";
+    $projects_result = mysqli_query($connection, $projects_sql);
+    
+    if (!$projects_result) {
+        $error = mysqli_error($connection);
+        echo 'Ошибка MySQL: ' . $error;
+    } else {
+        $projects_rows = mysqli_fetch_all($projects_result, MYSQLI_ASSOC);
+        echo print_r($projects_rows);
+    }
+
     $page_content = include_template('index.php', [
         'show_complete_tasks' => $show_complete_tasks,
         'tasks' => $tasks
@@ -17,10 +33,10 @@ if ($connection == false) {
 
     $layout_content = include_template('layout.php', [
         'content' => $page_content,
-        'projects' => $projects,
+        'projects' => $projects_rows,
         'tasks' => $tasks,
-        // 'title' => 'Дела в порядке'
-        'title' => $con_status
+        'title' => 'Дела в порядке'
+        // 'title' => $con_status
     ]);
 
     echo $layout_content;
--- templates/layout.php
+++ templates/layout.php
@@ -44,9 +44,9 @@
                     <ul class="main-navigation__list">
                         <?php foreach ($projects as $project): ?>
                             <li class="main-navigation__list-item">
-                                <a class="main-navigation__list-item-link" href="#"><?= escapeHtml($project); ?></a>
+                                <a class="main-navigation__list-item-link" href="#"><?= escapeHtml($project['name']); ?></a>
                                 <span class="main-navigation__list-item-count">
-                                    <?= calculateTasks($tasks, escapeHtml($project)); ?>
+                                    <?= calculateTasks($tasks, escapeHtml($project['name'])); ?>
                                 </span>
                             </li>
                         <?php endforeach; ?>
