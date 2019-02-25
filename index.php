3e701cf968c03f9600004cd7c89eb15d7b49bd76
nigel-riss <mr.kurenkov@gmail.com>
2019-02-25 16:35:48 +0200

    Add sql connection

--- index.php
+++ index.php
@@ -4,19 +4,26 @@ date_default_timezone_get('Etc/GMT');
 require_once('functions.php');
 require_once('data.php');
 
+$connection = mysqli_connect('localhost', 'root', '', 'doingsdone');
 
-$page_content = include_template('index.php', [
-    'show_complete_tasks' => $show_complete_tasks,
-    'tasks' => $tasks
-]);
+$con_status = '';
+if ($connection == false) {
+    echo 'Oшибка подключения: ' . mysqli_connect_error();
+} else {
+    $page_content = include_template('index.php', [
+        'show_complete_tasks' => $show_complete_tasks,
+        'tasks' => $tasks
+    ]);
 
-$layout_content = include_template('layout.php', [
-    'content' => $page_content,
-    'projects' => $projects,
-    'tasks' => $tasks,
-    'title' => 'Дела в порядке'
-]);
+    $layout_content = include_template('layout.php', [
+        'content' => $page_content,
+        'projects' => $projects,
+        'tasks' => $tasks,
+        // 'title' => 'Дела в порядке'
+        'title' => $con_status
+    ]);
 
-echo $layout_content;
+    echo $layout_content;
+}
 
 ?>
